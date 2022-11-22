ef97ae9537b365f0fe2cad822ae2c3bbb8a46279
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-22 13:12:17 +0430

    default codes removed and countries file created

---
+++ pages/countries.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-row>
+        <v-col cols="12">
+            <v-card elevation="4">
+                <v-card-title>
+                    Countries
+                </v-card-title>
+                <v-card-text>
+
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+<script>
+export default{
+    name:'countries'
+}
+</script>
