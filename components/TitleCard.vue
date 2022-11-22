8276ab474cbc171d477e173911eb21207579946c
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2022-11-22 20:36:06 +0430

    TitleCard component created

---
+++ components/TitleCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card elevation="4">
+    <v-card-title>
+      <v-icon>{{ title.icon }}</v-icon>
+      {{ title.title }}
+    </v-card-title>
+    <v-card-text>
+      <NuxtLink to="/" class="text-decoration-none">Dashboard</NuxtLink> >>
+      <NuxtLink :to="title.url" class="text-decoration-none">{{
+        title.title
+      }}</NuxtLink></v-card-text
+    >
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "BreadCrumb",
+  props: ["title"],
+  data() {
+    return {
+      title: {},
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/countries.vue
+++ pages/countries.vue
@@ -1,55 +1,55 @@
 <template>
-    <v-row>
-        <v-col cols="12">
-            <v-card elevation="4">
-                <v-card-title>
-                    <v-icon>mdi-flag</v-icon>
-                    Countries
-                </v-card-title>
-                <v-card-text>
-                    dashboar
-                </v-card-text>
-            </v-card>
-            <v-card elevation="4" class="mt-5">
-                <v-card-title>
-                    Countries
-                </v-card-title>
-                <v-card-text>
-                    <v-data-table :headers="headers" :items="countries" dense item-key="id" v-model="selected" show-select :single-select="singleSelect">
-                        <template v-slot:top>
-                            <v-switch label="Single Select" v-model="singleSelect"></v-switch>
-                        </template>
-                    </v-data-table>
-                </v-card-text>
-            </v-card>
-        </v-col>
-    </v-row>
+  <v-row>
+    <v-col cols="12">
+      <TitleCard :title="title_info" />
+      <v-card elevation="4" class="mt-5">
+        <v-card-text>
+          <v-data-table
+            :headers="headers"
+            :items="countries"
+            dense
+            item-key="id"
+            v-model="selected"
+            show-select
+            :single-select="singleSelect"
+          >
+            <template v-slot:top>
+              <v-switch label="Single Select" v-model="singleSelect"></v-switch>
+            </template>
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 <script>
-export default{
-    name:'countries',
-    data(){
-        return {
-            selected:[],
-            singleSelect:false,
-            countries:[
-                {id:1,code:'AFG',name:'Afghanistan',phonecode:'+93'}
-            ],
-            headers:[
-                {text:'ID',value:'id'},
-                {text:'Country Code',value:'code'},
-                {text:'Name',value:'name'},
-                {text:'Phone Code',value:'phonecode'},
-            ]
-        }
-    },
-    methods:{
-        getCountries(){
-
-        }
-    },
-    created(){
-        console.log('created');
-    }
-}
+import TitleCard from "~/components/TitleCard.vue";
+export default {
+  name: "countries",
+  components: {
+    TitleCard,
+  },
+  data() {
+    return {
+      title_info: { title: "Countries", icon: "mdi-flag", url: "countries" },
+      selected: [],
+      singleSelect: false,
+      countries: [
+        { id: 1, code: "AFG", name: "Afghanistan", phonecode: "+93" },
+      ],
+      headers: [
+        { text: "ID", value: "id" },
+        { text: "Country Code", value: "code" },
+        { text: "Name", value: "name" },
+        { text: "Phone Code", value: "phonecode" },
+      ],
+    };
+  },
+  methods: {
+    getCountries() {},
+  },
+  created() {
+    console.log("created");
+  },
+};
 </script>
