06d0e5ab26db93b80c2019268a03c4570bb6a864
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-01 11:20:16 +0430

    create modal added

--- pages/page_types.vue
+++ pages/page_types.vue
@@ -2,7 +2,7 @@
     <v-row>
         <v-col cols="12">
             <TitleCard :title_info="{title:'Page types', icon:'mdi-image-album',url:'page_types'}" />
-            <ActionsCard/>
+            <ActionsCard @openAddModal="openAddModal"/>
             <v-card>
                 <v-card-text>
                     <v-data-table :headers="headers" :items="page_types" show-select v-model="selected" :single-select="single_select" item-key="id" dense :search="single_search">
@@ -16,6 +16,21 @@
                 </v-card-text>
             </v-card>
         </v-col>
+        <v-dialog v-model="add_dialog" width="400">
+            <v-card>
+                <v-form class="pa-4">
+                    <v-card-title>Create new page type</v-card-title>
+                    <v-card-text>
+                        <v-text-field lable="Name" placeholder="Enter name here" v-model="page_type.name" dense outlined rounded></v-text-field>
+                        <v-select v-model="page_type.company_id" :items="companies" item-text="name" item-value="id" rounded outlined dense></v-select>
+                    </v-card-text>
+                    <v-card-actions class="d-flex justify-end">
+                        <v-btn small>Cancel</v-btn>
+                        <v-btn small color="primary">Save</v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card>
+        </v-dialog>
     </v-row>
 </template>
 <script>
@@ -24,7 +39,10 @@ export default {
         return{
             single_select:false,
             single_search: "",
+            add_dialog:false,
             selected:[],
+            companies:[],
+            page_type:{id:null,name:'',company_id:null},
             headers:[
                 {text:"ID", value: "id"},
                 {text:"Name", value: "name"},
@@ -40,6 +58,17 @@ export default {
             }).catch(error=>{
                 console.log(error);
             })
+        },
+        getCompanies(){
+            this.$axios.get('company').then(response=>{
+                this.companies=response.data;
+            }).catch(error=>{
+                console.log(error);
+            })
+        },
+        openAddModal() {
+            this.add_dialog=true;
+            this.getCompanies();
         }
     },
     created() {this.index();}
