2d1f09d21b88b5dff16405368ae8c96383d57082
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-04 15:42:53 +0430

    title and actions card added

--- pages/templates.vue
+++ pages/templates.vue
@@ -1,12 +1,30 @@
 <template>
     <v-row>
         <v-col cols="12">
-            lorem
+            <TitleCard :title_info="{title:'Templates',icon:'mdi-credit-card-chip-outline',url:'templates'}"/>
+            <ActionsCard/>
         </v-col>
     </v-row>
 </template>
 <script>
 export default {
-    
+    data(){
+        return {
+            templates:[]
+        }
+    },
+    methods: {
+        index() {
+            this.$axios.get('template').then(response=>{
+                this.templates=response.data;
+                console.log(this.templates);
+            }).catch(error=>{
+                console.log(error);
+            });
+        }
+    },
+    created() {
+        this.index();
+    }
 }
 </script>
