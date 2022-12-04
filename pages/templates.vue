e2502f54de9978398886f4600bf2a92c16109e8a
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-04 16:07:43 +0430

    read operation done, data added to datatable

--- pages/templates.vue
+++ pages/templates.vue
@@ -3,6 +3,19 @@
         <v-col cols="12">
             <TitleCard :title_info="{title:'Templates',icon:'mdi-credit-card-chip-outline',url:'templates'}"/>
             <ActionsCard/>
+            <v-card>
+                <v-card-text>
+                    <v-data-table :items="templates" :headers="headers" show-select :single-select="single_select" v-model="selected" :search="single_search" dense>
+                        <template v-slot:top label="Single Select">
+                            <v-switch v-model="single_select"></v-switch>
+                        </template>
+                        <template v-slot:item.page_type_id="{item}">
+                            {{item.page_type.name}}
+                        </template>
+                        <template v-slot:item.image="{ item }"><v-img :src="item.image" width="60" rounded></v-img></template>
+                    </v-data-table>
+                </v-card-text>
+            </v-card>
         </v-col>
     </v-row>
 </template>
@@ -10,7 +23,11 @@
 export default {
     data(){
         return {
-            templates:[]
+            templates:[],
+            headers: [{text:'ID',value:'id'},{text:'Name',value:'name'},{text:'Image',value:'image'},{text:'Page Type',value:'page_type_id'}],
+            single_select: false,
+            selected:[],
+            single_search: "",
         }
     },
     methods: {
