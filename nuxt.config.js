46f37a2c0ca44b33cb4492400592d1219089b342
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-04 15:19:44 +0430

    index.js file created

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {src:'~/plugins/toastr.js'},
-    {src:'~/plugins/sweetalert2.js'}
+    {src:'~/plugins/sweetalert2.js'},
+    {src:'~/plugins/index.js'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
