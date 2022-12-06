56b6a3e1e64328989197c3bdbc451ebf67858a1e
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-06 16:23:29 +0430

    landing_pages buiding block done

---
+++ pages/landing_pages.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-row>
+        <v-col cols="12">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis animi, incidunt at commodi dicta libero, error iure atque quod quidem, illo magnam ea? Ab aliquam cum minima quae quos neque?
+        </v-col>
+    </v-row>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
