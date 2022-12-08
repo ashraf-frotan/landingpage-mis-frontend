d19dc366eebd8be9f732efae9443fede35ea06bb
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-08 11:40:25 +0430

    stepper changed to vertical

--- pages/landing_pages.vue
+++ pages/landing_pages.vue
@@ -32,100 +32,102 @@
         </v-toolbar>
       </v-card>
       <v-card>
-        <v-stepper v-model="e1">
-          <v-stepper-header>
-            <v-stepper-step :complete="e1 > 1" step="1" editable>
-              Step 1
-            </v-stepper-step>
-            <v-stepper-step :complete="e1 > 2" step="2" editable>
-              Step 2
-            </v-stepper-step>
-            <v-stepper-step :complete="e1 > 3" step="3" editable>
-              Step 3
-            </v-stepper-step>
-          </v-stepper-header>
-          <v-stepper-items>
-            <v-stepper-content step="1">
-              <div>
-                <v-row>
-                  <v-col cols="12" md="6" sm="12" xs="12">
-                    <v-select
-                      :items="countries"
-                      item-text="name"
-                      item-value="id"
-                      rounded
-                      outlined
-                      dense
-                      label="Country"
-                      placeholder="Please select country"
-                    >
-                    </v-select>
-                  </v-col>
-                  <v-col cols="12" md="6" sm="12" xs="12">
-                    <v-select
-                      :items="companies"
-                      item-text="name"
-                      item-value="id"
-                      rounded
-                      outlined
-                      dense
-                      label="Company"
-                      placeholder="Please select company"
-                    >
-                    </v-select>
-                  </v-col>
-                  <v-col cols="12" md="6" sm="12" xs="12">
-                    <v-select
-                      :items="page_types"
-                      item-text="name"
-                      item-value="id"
-                      rounded
-                      outlined
-                      dense
-                      label="Landing Pages"
-                      placeholder="Please select landing page"
-                    >
-                    </v-select>
-                  </v-col>
-                  <v-col cols="12" md="6" sm="12" xs="12">
-                    <v-select
-                      :items="templates"
-                      item-text="name"
-                      item-value="id"
-                      rounded
-                      outlined
-                      dense
-                      label="Templates"
-                      placeholder="Please select templates"
-                    >
-                    </v-select>
-                  </v-col>
-                </v-row>
-              </div>
-              <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
-              <v-btn text> Cancel </v-btn>
-            </v-stepper-content>
-            <v-stepper-content step="2">
-              <v-card class="mb-12">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos
-                ipsam amet nesciunt. Dicta neque libero excepturi, rem,
-                suscipit, minima voluptatibus aliquam ad quam sint alias et esse
-                error aliquid nisi?
-              </v-card>
-              <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
-              <v-btn text> Cancel </v-btn>
-            </v-stepper-content>
-            <v-stepper-content step="3">
-              <v-card class="mb-12">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos
-                ipsam amet nesciunt. Dicta neque libero excepturi, rem,
-                suscipit, minima voluptatibus aliquam ad quam sint alias et esse
-                error aliquid nisi?
-              </v-card>
-              <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
-              <v-btn text> Cancel </v-btn>
-            </v-stepper-content>
-          </v-stepper-items>
+        <v-stepper
+          v-model="e1"
+          vertical
+        >
+          <v-stepper-step
+            :complete="e1 > 1"
+            step="1"
+          >
+            Select an app
+            <small>Summarize if needed</small>
+          </v-stepper-step>
+
+          <v-stepper-content step="1">
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn
+              color="primary"
+              @click="e1 = 2"
+            >
+              Continue
+            </v-btn>
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-step
+            :complete="e1 > 2"
+            step="2"
+          >
+            Configure analytics for this app
+          </v-stepper-step>
+
+          <v-stepper-content step="2">
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn
+              color="primary"
+              @click="e1 = 3"
+            >
+              Continue
+            </v-btn>
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-step
+            :complete="e1 > 3"
+            step="3"
+          >
+            Select an ad format and name ad unit
+          </v-stepper-step>
+
+          <v-stepper-content step="3">
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn
+              color="primary"
+              @click="e1 = 4"
+            >
+              Continue
+            </v-btn>
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-step step="4">
+            View setup instructions
+          </v-stepper-step>
+          <v-stepper-content step="4">
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn
+              color="primary"
+              @click="e1 = 1"
+            >
+              Continue
+            </v-btn>
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
         </v-stepper>
       </v-card>
     </v-dialog>
