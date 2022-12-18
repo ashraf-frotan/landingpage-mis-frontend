234c4be2691ae6f0487bca27265208482e1dc001
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-18 15:31:26 +0430

    oredoh logo added

--- components/landing_page/Logo.vue
+++ components/landing_page/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row align="center">
     <v-col cols="12" class="d-flex justify-center py-6">
-      <img src="~/assets/images/uae.png" alt="" width="80" />
+      <img src="~/assets/images/oredoh.png" alt="" width="120" />
     </v-col>
   </v-row>
 </template>
