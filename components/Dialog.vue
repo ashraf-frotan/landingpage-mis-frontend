5d0c3bd3858164a45b89a0d98b5b4925a67dc289
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-26 11:34:45 +0430

    v-dialog added

--- components/Dialog.vue
+++ components/Dialog.vue
@@ -1,8 +1,19 @@
 <template>
-    
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus quam, veniam dolor voluptatem veritatis recusandae corporis magni tenetur? Laudantium sint illum minima sequi placeat eligendi doloremque numquam obcaecati voluptas quam.
+    </v-dialog>
 </template>
 <script>
 export default {
-    
+    data(){
+        return {
+            dialog:true
+        }
+    }
 }
 </script>
