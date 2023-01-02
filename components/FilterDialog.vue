a2aabb083925459242be1264634613f39267699b
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2023-01-02 22:49:57 +0430

    variables added

--- components/FilterDialog.vue
+++ components/FilterDialog.vue
@@ -4,10 +4,17 @@
       <v-card>
         <v-card-title> Filter Products </v-card-title>
         <v-card-text>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia,
-          consequatur minima sint ex porro amet. Deserunt, quaerat architecto
-          dignissimos labore aliquam, magni laborum incidunt, maiores quam
-          nesciunt suscipit blanditiis laudantium?
+          <v-row>
+            <v-col cols="12" md="4" sm="4" xs="4"
+              ><v-select dense></v-select>
+            </v-col>
+            <v-col cols="12" md="4" sm="4" xs="4">
+              <v-select dense></v-select>
+            </v-col>
+            <v-col cols="12" md="4" sm="4" xs="4">
+              <v-select dense></v-select>
+            </v-col>
+          </v-row>
         </v-card-text>
         <v-card-actions class="d-flex justify-end">
           <v-btn small class="text-capitalize"> Cancel </v-btn>
@@ -23,6 +30,15 @@ export default {
   data() {
     return {
       filter_dialog: true,
+      landing_info: {
+        countries: [],
+        companies: [],
+        templates: [],
+        pcode: "",
+        title: "",
+        sale_type: "",
+        page_type: "",
+      },
     };
   },
 };
