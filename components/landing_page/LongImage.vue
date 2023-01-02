b6970a45808e59c29742832c03d29ccb16d9d39d
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2023-01-02 10:17:10 +0430

    div used instead of template in for loop

--- components/landing_page/LongImage.vue
+++ components/landing_page/LongImage.vue
@@ -1,11 +1,9 @@
 <template>
   <v-row>
     <v-col cols="12" md="12" class="pa-0">
-      <template v-for="(image, i) in l_images">
-        <div  :key="i">
+      <div v-for="(image, i) in l_images" :key="i">
           <v-img :src="`${baseUrl}assets/images/products/${pcode}${image.name}`" width="100%"  alt="" />
         </div>
-      </template>
     </v-col>
   </v-row>
 </template>
