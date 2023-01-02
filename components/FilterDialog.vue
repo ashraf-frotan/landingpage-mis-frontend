59c4c2038f8e4ee592dab95c9832467b4382cd78
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2023-01-02 23:11:54 +0430

    Countries, Companies, Templates added to filter dialog

--- components/FilterDialog.vue
+++ components/FilterDialog.vue
@@ -6,13 +6,30 @@
         <v-card-text>
           <v-row>
             <v-col cols="12" md="4" sm="4" xs="4"
-              ><v-select dense></v-select>
+              ><v-select
+                dense
+                :items="countries"
+                item-text="name"
+                item-value="id"
+                @change="getCompanies($event)"
+              ></v-select>
             </v-col>
             <v-col cols="12" md="4" sm="4" xs="4">
-              <v-select dense></v-select>
+              <v-select
+                dense
+                :items="companies"
+                item-text="name"
+                item-value="id"
+                @change="getTemplates($event)"
+              ></v-select>
             </v-col>
             <v-col cols="12" md="4" sm="4" xs="4">
-              <v-select dense></v-select>
+              <v-select
+                dense
+                :items="templates"
+                item-text="name"
+                item-value="id"
+              ></v-select>
             </v-col>
           </v-row>
         </v-card-text>
@@ -30,16 +47,50 @@ export default {
   data() {
     return {
       filter_dialog: true,
+      data: {},
+      countries: [],
+      companies: [],
+      templates: [],
       landing_info: {
-        countries: [],
-        companies: [],
-        templates: [],
+        country_id: "",
+        company_id: "",
+        template_id: "",
         pcode: "",
         title: "",
         sale_type: "",
         page_type: "",
       },
     };
+  },
+  methods: {
+    getInfo() {
+      this.$axios
+        .get("get_info")
+        .then((response) => {
+          this.data = response.data;
+          this.countries = this.data.countries;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getCompanies(id) {
+      this.landing_info.country_id = id;
+      console.log(id);
+      this.companies = this.data.companies.filter((el) => {
+        return el.country_id == id;
+      });
+      this.templates = [];
+    },
+    getTemplates(id) {
+      this.landing_info.company_id = id;
+      this.templates = this.data.templates.filter((el) => {
+        return el.id == id;
+      });
+    },
+  },
+  created() {
+    this.getInfo();
   },
 };
 </script>
