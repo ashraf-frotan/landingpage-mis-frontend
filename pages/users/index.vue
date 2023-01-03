22894d8e1c1916c4e0dfdc29f9b1871d6486c0b2
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2023-01-03 16:29:51 +0430

    title and actions components added to users  page

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sint debitis facere voluptatem dolores id deleniti harum cupiditate ducimus quos illum nostrum nulla eveniet fuga, at nihil laborum aperiam consequuntur.
+        <TitleCard :title_info="{
+          title: 'Users',
+          icon: 'mdi-account-multiple',
+          url: 'users',
+        }" />
+        <ActionsCard/>     
     </div>
 </template>
 <script>
