9845bdb2b66f31a15461865ced52ca17b6f91080
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2023-01-05 09:32:13 +0430

    input types added

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row align="center" style="height:100vh;">
         <v-col cols="12" class="d-flex justify-center">
-                <v-card class="" style="max-width:460px" elevation="3">
+                <v-card class="" style="width:460px" elevation="3">
                     <v-card-title>
                         <h2>Welcome</h2>
                         
@@ -15,7 +15,8 @@
                         <v-row>
                             <v-col cols="12">
                                 <v-form>
-                                    
+                                    <v-text-field outlined rounded dense label="Email" placeholder="Enter email here" type="email"></v-text-field>
+                                    <v-text-field outlined rounded dense label="Passwrd" placeholder="Enter password here" type="password"></v-text-field>
                                 </v-form>
                             </v-col>
                         </v-row>
